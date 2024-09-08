694a6cf335af7d084d52a096f4efbe164a74dbbc
guoguo <576034485@qq.com>
2024-09-09 00:37:17 +0800

    day11

---
+++ project01/day11/main/EthBalance.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"log"
+	"math"
+	"math/big"
+)
+
+func main() {
+	client, err := ethclient.Dial("http://localhost:8545")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	account := common.HexToAddress("0x6B48e176dc7B17C4712E3C460402fDC8F2222F3E")
+	balance, err := client.BalanceAt(context.Background(), account, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(balance) // 25893180161173005034
+
+	blockNumber := big.NewInt(5532993)
+	balanceAt, err := client.BalanceAt(context.Background(), account, blockNumber)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(balanceAt) // 25729324269165216042
+
+	fbalance := new(big.Float)
+	fbalance.SetString(balanceAt.String())
+	ethValue := new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
+	fmt.Println(ethValue) // 25.729324269165216041
+
+	pendingBalance, err := client.PendingBalanceAt(context.Background(), account)
+	fmt.Println(pendingBalance) // 25729324269165216042
+}
